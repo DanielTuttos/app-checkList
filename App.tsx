fde526a8325a802bb5a5b650c62f3f6a417166a2
Daniel Romero <tutorialesdanielromero@gmail.com>
2023-11-16 20:34:56 -0500

    :fire: eliminando codigo comentado

--- App.tsx
+++ App.tsx
@@ -1,10 +1,8 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {DbContextProvider} from './src/context/DBContext';
-// import {SQLiteDatabase} from 'react-native-sqlite-storage';
 import {Home} from './src/screens';
 import {NavigationContainer} from '@react-navigation/native';
-import {TabNavigator} from './src/navigator';
 import {SafeAreaView} from 'react-native';
 
 function App(): JSX.Element {
