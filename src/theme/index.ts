47a73755b3a78feac566e10405fe9c859645251e
Daniel Romero <tutorialesdanielromero@gmail.com>
2023-11-17 20:30:27 -0500

    :construction_worker: agregando colores de tema

---
+++ src/theme/index.ts
@@ -0,0 +1,8 @@
+export const colors = {
+  primary: '#7858f2', // buttons
+  secondary: '#EDF2FA', // background o text
+  backgroundPrimary: '#ffffff', // background o text
+  backgroundSecondary: '#09184d', // background menu
+  textPrimary: '#2F2E2E', // titles
+  textSecondary: '#808085', // info
+};
